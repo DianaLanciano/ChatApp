b4ba09267129e93fd32707c5fd7790ea23ca7a59
Diana Lanciano <dianalanciano@Dianas-Laptop.local>
2024-05-18 18:57:10 +0300

    added conection to mongo

--- server/db/mongoConnectionSetup.js
+++ server/db/mongoConnectionSetup.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const establishMongoConnection = async () => {
     try {
         await mongoose.connect(process.env.MONGO_DB_URI);
+        console.log('Connection to mongoDB has established');
     } catch (error) {
         console.log('An error occurred while trying to connect mongoDB', error.message);
     }
