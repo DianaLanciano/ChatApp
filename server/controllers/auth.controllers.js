398af381d6a5987387b311eec82433e1faec8a23
Diana Lanciano <dianalanciano@Dianas-Laptop.local>
2024-05-19 11:41:05 +0300

    implemanted login logout signup controllers

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -41,14 +41,39 @@ export const signup = async (req, res) => {
         
    } catch (error) {
     console.log('Error in signup', error.message);
-    //res.send(500).json({error:"Server error while trying to signup"});
+    res.status(500).json({error:"Server error while trying to signup"});
    }
 };
 
-export const login = (req, res) => {
-    console.log('login');
+export const login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({username});
+        const isPasswordValid = await bcrypt.compare(password, user?.password || "");
+        if (!user || !isPasswordValid) {
+            return res.status(400).json({error:"Invalid username or password"});
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+        res.status(201).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            profilePicture: user.profilePicture
+        });
+
+    } catch (error) {
+        console.log('Error in login', error.message);
+        res.status(500).json({error:"Server error while trying to login"});
+    }
 };
 
 export const logout = (req, res) => {
-    console.log('logout');
+    try {
+        res.cookie("jwt", "", {maxAge: 0});
+        res.status(200).json({message: "Logged out successfully"});
+    } catch (error) {
+        console.log('Error in logout', error.message);
+        res.status(500).json({error:"Server error while trying to logout"});
+    }
 };
