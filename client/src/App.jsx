ec863f51719d524f46d0b4558c871dbe351a862a
Diana Lanciano <dianalanciano@Dianas-Laptop.local>
2024-05-20 21:47:51 +0300

    added react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/Signup";
-import Home from "./pages/home/Home";
-import MessageContainer from './components/messages/MessageContainer';
 
 function App() {
-
   return (
- <div className='p-4 h-screen flex items-center justify-center'>
-  <Home />
- </div>
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </div>
   );
 }
 
