97f7f13b11dfe87ca495c25a2969f36a548e9cba
Diana Lanciano <dianalanciano@Dianas-Laptop.local>
2024-05-29 00:05:06 +0300

    fixed build

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import cookieParser from 'cookie-parser';
 
 import authRoutes from './routs/auth.routes.js';
@@ -11,18 +10,13 @@ import establishMongoConnection from './db/mongoConnectionSetup.js'
 import {  app, server } from './socket/socket.js';
 
 
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 dotenv.config();
-//const __dirname = path.resolve(); // gives root directory name
-const PORT = process.env.PORT;
+const __dirname = path.resolve(); // gives root directory name
+const PORT = process.env.PORT || 5000;
 
 /**************************************  UTILS ************************************* */
 app.use(express.json()); // to parse incoming requests 
 app.use(cookieParser()); // to get token for example from user
-app.use(express.static);
 
 
 /**************************************  MIDDLEWARE ************************************* */
@@ -32,12 +26,10 @@ app.use("/api/users", userRoutes);
 
 
 /**************************************  DEPLOY CONFIGURATION ************************************* */
-console.log('path.join(__dirname, "/client/dist")', path.join(__dirname, "/client/dist"));
 /*used to serve static files the we have in client (such as HTML, CSS, JavaScript, images, and other assets)*/
 app.use(express.static(path.join(__dirname, "/client/dist"))); 
 
 app.get('*', (req, res) => {
-    console.log("we here");
     res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
